c7b500c15828666ed7dbd3fac16622b58aa43854
hykimyong <hykimyong@naver.com>
2023-09-28 03:39:25 +0900

    css 수정

--- src/pages/Bjscreen.tsx
+++ src/pages/Bjscreen.tsx
@@ -4,34 +4,33 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { FormLabel } from '@mui/material';
 
 const Base = styled.div`
-  height: 333.6px; 
-  width: 250.2px; 
-  transform: scale(1.036);
+  text-align: center;
 `;
 
 export default function Bjscreen() {
   return (
     <Base>
+      <FormLabel>BJ아이디 등록하기</FormLabel>
       <Box
         component="form"
         sx={{
           '& > :not(style)': { m: 1},
           display: 'flex',
           alignItems: 'center',
-          width: '100px'
+          justifyContent:'center'
         }}
         noValidate
         autoComplete="off"
       >
-        <TextField id="standard-basic" label="BjId" variant="standard" />
-        
+        <TextField id="standard-basic" label="아이디" variant="standard" />
+        <TextField id="standard-basic" label="닉네임" variant="standard" />
+        <Button variant="contained">추가</Button>  
       </Box>
-      <Button variant="contained">추가</Button>
-      <Stack>
-        
-      </Stack>
+      <br/><br/>
+      <FormLabel>등록된 ID</FormLabel>
     </Base>
 
   );
