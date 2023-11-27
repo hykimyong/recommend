7f44b43ffee2782ddaa21a9b214048e004c845d0
hykimyong <hykimyong@naver.com>
2023-11-27 21:33:33 +0900

    훅 수정

--- src/hook/useSearchData.ts
+++ src/hook/useSearchData.ts
@@ -1,12 +1,9 @@
 import { useQuery, UseQueryResult, QueryObserverSuccessResult } from 'react-query';
+import { AxiosResponse } from 'axios';
 import { SearchApi } from '../apis/searchApi';
 import { ISearch } from '../types/search';
 
-
-const useSearchData = (keyword:string): UseQueryResult<ISearch, Error> => {
-  const result = useQuery<ISearch, Error>(`search${keyword}`, SearchApi(keyword));
-    console.log(result);
-  return result as QueryObserverSuccessResult<ISearch, Error>;
-};
+const useSearchData = (keyword:string) =>
+  useQuery<AxiosResponse<ISearch>, Error>(['species', { keyword }], () => SearchApi(keyword));
 
 export default useSearchData;
