d2a9e0238e46bb66792e3fd3aa086f48d7e1741c
hykimyong <hykimyong@naver.com>
2023-11-29 22:19:27 +0900

    주석제거

--- src/hook/useSearchData.ts
+++ src/hook/useSearchData.ts
@@ -8,7 +8,7 @@ useQuery<AxiosResponse<ISearch>, Error>(
   ['search', { keyword }],
   () => (keyword ? SearchApi(keyword) : Promise.reject('Keyword is empty')),
   {
-    enabled: keyword !== '' && keyword !== undefined, // 키워드가 비어 있거나 정의되지 않았을 때 API 호출 방지
+    enabled: keyword !== '' && keyword !== undefined,
   }
 );
 
