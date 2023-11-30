90f903aa9f0d2df50bc326169f33a04dc30a3d31
hykimyong <hykimyong@naver.com>
2023-11-30 21:28:53 +0900

    type지정
    https://app.quicktype.io/이용

--- src/types/search.ts
+++ src/types/search.ts
@@ -1,3 +1,25 @@
-export  interface ISearch {
-    
+export interface ISearch {
+    result:           number;
+    history:          History;
+    suggest_bj:       SuggestBj[];
+    suggest_contents: string[];
+    charset:          string;
+    t:                string;
+}
+
+export interface History {
+    isActive: number;
+    list:     List;
+    d:        string;
+}
+
+export interface List {
+}
+
+export interface SuggestBj {
+    user_id:      string;
+    user_nick:    string;
+    station_logo: string;
+    medal:        boolean;
+    broad_no:     string;
 }
